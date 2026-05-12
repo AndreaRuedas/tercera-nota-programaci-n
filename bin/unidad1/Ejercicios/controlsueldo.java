fa1425e1d2e97530353593c1585f5df75ee2b72e
AndreaRuedas <adruedasr@ufpso.edu.co>
2026-05-11 23:24:12 -0500

    se agrega

---
+++ bin/unidad1/Ejercicios/controlsueldo.java
@@ -0,0 +1,29 @@
+public class controlsueldo {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cantEmpleados, cantEntre100y300 = 0, cantMayor300 = 0;
+        double sueldo, importeTotal = 0;
+ 
+        System.out.print("Ingrese la cantidad de empleados: ");
+        cantEmpleados = sc.nextInt();
+ 
+        for (int i = 1; i <= cantEmpleados; i++) {
+            System.out.print("Ingrese el sueldo del empleado " + i + ": ");
+            sueldo = sc.nextDouble();
+ 
+            importeTotal += sueldo;
+ 
+            if (sueldo > 300) {
+                cantMayor300++;
+            } else {
+                cantEntre100y300++;
+            }
+        }
+ 
+        System.out.println("Empleados con sueldo entre $100 y $300: " + cantEntre100y300);
+        System.out.println("Empleados con sueldo mayor a $300: " + cantMayor300);
+        System.out.println("Importe total en sueldos: $" + importeTotal);
+ 
+        sc.close();
+    }
+}
